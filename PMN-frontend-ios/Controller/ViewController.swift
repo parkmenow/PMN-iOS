bf737b06a5cb50cc2c18b2c7d2f03c52463d9e5c
bharath <bharath@mercari.com>
2019-02-16 08:33:37 +0900

    Initial Commit

---
+++ PMN-frontend-ios/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PMN-frontend-ios
+//
+//  Created by bharath on 2019/02/16.
+//  Copyright © 2019 bharath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
